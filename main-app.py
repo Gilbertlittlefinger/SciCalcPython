9266c27a66d79e8bba9c3e839240e97814267874
david <dburcad@gmail.com>
2026-03-01 12:47:17 -0500

    Added operation format look for equation

--- main-app.py
+++ main-app.py
@@ -25,6 +25,7 @@ def displayResult(x: float, mode: str):
 
 def performCalcLoop(calc, dis):
     result = 0
+    result_format = ""
     memory = 0
     switch = "decimal"  # Default display mode
     print("\nWelcome to the Scientific Calculator!", end="")
@@ -49,7 +50,7 @@ def performCalcLoop(calc, dis):
                     print("\nCommand not recognized. Rerunning user_interface.exe, please try again.")
     
         elif choice == 'help':
-            print("\nAvailable operations: add, sub, mult, div, sqr, root2, exp, inv, neg, sin, cos, tan, isin, icos, itan, deg, rad, fac")
+            print("\nAvailable operations: add, sub, mult, div, sqr, root2, exp, inv, neg, sin, cos, tan, isin, icos, itan, deg, rad, fac, log, log10, natlog, in_natlog, infinity, abs_val")
             print("Memory operations: M+ (store current result in memory), MC (clear memory), MRC (recall memory)")
             print("Display mode operations: hexadecimal, binary, decimal, octal")
             print("Other operations: clear (clears the display), q (quit the calculator)")
@@ -60,100 +61,148 @@ def performCalcLoop(calc, dis):
 
         # Implementing calculator operations
         elif choice == 'add':
+            print("\nFormat: [number1] + [number2]")
             a, b = getTwoNumbers()
             result = calc.add(a, b)
+            result_format = f"{a} + {b} = {result}"
 
         elif choice == 'sub':
+            print("\nFormat: [number1] - [number2]")
             a, b = getTwoNumbers()
             result = calc.sub(a, b)
+            result_format = f"{a} - {b} = {result}"
 
         elif choice == 'mult':
+            print("\nFormat: [number1] * [number2]")
             a, b = getTwoNumbers()
             result = calc.mult(a, b)
+            result_format = f"{a} * {b} = {result}"
 
         elif choice == 'div':
+            print("\nFormat: [number1] / [number2]")
             a, b = getTwoNumbers()
             result = calc.div(a, b)
+            result_format = f"{a} / {b} = {result}"
 
         elif choice == 'sqr':
+            print("\nFormat: [number] ^ 2")
             a = getOneNumber()
             result = calc.sqr(a)
+            result_format = f"{a} ^ 2 = {result}"
 
         elif choice == 'root2':
+            print("\nFormat: sqrt([number])")
             a = getOneNumber()
             result = calc.root2(a)
+            result_format = f"sqrt({a}) = {result}"
 
         elif choice == 'exp':
+            print("\nFormat: [number1] ^ [number2]")
             a, b = getTwoNumbers()
             result = calc.exp(a, b)
+            result_format = f"{a} ^ {b} = {result}"
 
         elif choice == 'inv':
+            print("\nFormat: 1 / [number]")
             a = getOneNumber()
             result = calc.inv(a)
+            result_format = f"1 / {a} = {result}"
 
         elif choice == 'neg':
+            print("\nFormat: -[number]")
             a = getOneNumber()
             result = calc.neg(a)
+            result_format = f"-{a} = {result}"
 
         elif choice == 'sin':
+            print("\nFormat: sin([number])")
             a = getOneNumber()
             result = calc.sin(a)
+            result_format = f"sin({a}) = {result}"
 
         elif choice == 'cos':
+            print("\nFormat: cos([number])")
             a = getOneNumber()
             result = calc.cos(a)
+            result_format = f"cos({a}) = {result}"
 
         elif choice == 'tan':
+            print("\nFormat: tan([number])")
             a = getOneNumber()
             result = calc.tan(a)
+            result_format = f"tan({a}) = {result}"
 
         elif choice == 'isin':
+            print("\nFormat: arcsin([number])")
             a = getOneNumber()
             result = calc.isin(a)
+            result_format = f"arcsin({a}) = {result}"
 
         elif choice == 'icos':
+            print("\nFormat: arccos([number])")
             a = getOneNumber()
             result = calc.icos(a)
+            result_format = f"arccos({a}) = {result}"
 
         elif choice == 'itan':
+            print("\nFormat: arctan([number])")
             a = getOneNumber()
             result = calc.itan(a)
+            result_format = f"arctan({a}) = {result}"
 
         elif choice == 'deg':
+            print("\nFormat: deg([number])")
             a = getOneNumber()
             result = calc.deg(a)
+            result_format = f"deg({a}) = {result}"
 
         elif choice == 'rad':
+            print("\nFormat: rad([number])")
             a = getOneNumber()
             result = calc.rad(a)
+            result_format = f"rad({a}) = {result}"
 
         elif choice == 'fac':
+            print("\nFormat: [number]!")
             a = getOneNumber()
             result = calc.fac(a)
+            result_format = f"{a}! = {result}"
 
         elif choice == 'log':
+            print("\nFormat: log([number])")
             a = getOneNumber()
             result = calc.log(a)
+            result_format = f"log({a}) = {result}"
 
         elif choice == 'log10':
+            print("\nFormat: log10([number])")
             a = getOneNumber()
             result = calc.log10(a)
+            result_format = f"log10({a}) = {result}"
 
         elif choice == 'natlog':
+            print("\nFormat: ln([number])")
             a = getOneNumber()
             result = calc.natlog(a)
+            result_format = f"ln({a}) = {result}"
 
         elif choice == 'in_natlog':
+            print("\nFormat: in_ln([number])")
             a = getOneNumber()
             result = calc.in_natlog(a)
+            result_format = f"in_ln({a}) = {result}"
 
         elif choice == 'infinity':
+            print("\nFormat: infinity([number])")
             a = getOneNumber()
             result = calc.infinity(a)
+            result_format = f"infinity({a}) = {result}"
 
         elif choice == 'abs_val':
+            print("\nFormat: abs([number])")
             a = getOneNumber()
             result = calc.abs_val(a)
+            result_format = f"abs({a}) = {result}"
 
         # Passes or invalid imput
         elif choice.lower() == "m+" or choice.lower() == "mc" or choice.lower() == "mrc" \
@@ -165,7 +214,8 @@ def performCalcLoop(calc, dis):
 
         print(f"\nCurrent decimal memory value: {memory}")
         print(f"Current display mode: {switch}\n")
-        print("Result: ", end="")
+        print(f"{result_format}")
+        print("Displaying result in current display mode: ", end="")
         displayResult(result, switch)
         
 
